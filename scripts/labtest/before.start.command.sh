72a9b84bd6ba09e525df2e55f983b062a82072d1
David <david.upton@boston.gov>
2018-06-01 14:02:20 -0400

    Extends the before start script to install required modules.

--- scripts/labtest/before.start.command.sh
+++ scripts/labtest/before.start.command.sh
@@ -3,4 +3,13 @@
 # Runs at the end of the labtest process.
 
 cd docroot
-drush en -y labtest
+#-> Make a space separated list of modules to enable here
+MODULES="labtest"
+
+drush en $MODULES -y
+
+#-> Run any hook updates
+drush updb -y
+
+#-> Reset the admin password for this experiement
+drush upwd admin --password="admin" -y
